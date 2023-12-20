20027e4b742f7b11592c1236c0f28588efbbd3d6
Leow Fu Lin <fulin.leow@gmail.com>
2023-12-20 19:07:40 +0800

    created roomListingPage

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -1,14 +1,22 @@
 'use client';
 import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { TableHead, TableRow, TableHeader, TableCell, TableBody, Table } from "@/components/ui/table"
 import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
-import { createRoom } from "@/lib/roomService"
+import { PagingRoom, Room, createRoom, getRooms } from "@/lib/roomService"
 import { LocalDateTime } from "local-date";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Component() {
+    const router = useRouter();
+    const [rooms, setRooms] = useState<Room[]>([]);
+    const [hasNextPage, setHasNextPage] = useState(false);
+    const [hasPrevPage, setHasPrevPage] = useState(false);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalPage, setTotalPage] = useState(0);
 
     const createRoomHandler = async () => {
         const resp = await createRoom({
@@ -17,17 +25,45 @@ export default function Component() {
         })
     };
 
+    const turnPageHandler = (action: string) => {
+        if (action == "next") {
+            setCurrentPage(currentPage + 1);
+        } else if (action == "prev") {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+
+    const roomJoinHandler = (roomId: Number) => {
+        router.push(`/room/${roomId}`);
+    }
+
+    const getRoomsHandler = useEffect(() => {
+        const fetchData = async () => {
+            //currently didnt use size as table UI component didnt come by default for changing
+            //size of the table
+            const pagingRoomResp: PagingRoom = await getRooms({ page: currentPage, size: 10 });
+
+            console.log(pagingRoomResp);
+            setRooms(pagingRoomResp.content);
+            setHasNextPage(pagingRoomResp.last);
+            setHasPrevPage(pagingRoomResp.first);
+            setTotalPage(pagingRoomResp.totalPages);
+        }
+
+        fetchData();
+    }, [currentPage]);
+
     return (
         <main className="p-6 space-y-6">
 
             <Card className=" w-full mx-auto">
                 <CardHeader className="flex flex-row justify-between items-center">
                     <div className="flex items-center">
-                        <CardTitle>Chatroom Records</CardTitle>
+                        <CardTitle>Lunch GoWHERE</CardTitle>
                     </div>
                     <div className="flex items-center gap-4">
                         <Button size="sm" onClick={createRoomHandler}>
-                            Create Gathering Room
+                            Jio Ppl for lunch
                         </Button>
                     </div>
                 </CardHeader>
@@ -40,33 +76,45 @@ export default function Component() {
                                 <TableHead>Description</TableHead>
                                 <TableHead>Created By</TableHead>
                                 <TableHead>Active</TableHead>
+                                <TableHead>Target Time</TableHead>
                             </TableRow>
                         </TableHeader>
                         <TableBody>
-                            <TableRow>
-                                <TableCell className="font-medium">Chatroom 1</TableCell>
-                                <TableCell>Description for Chatroom 1</TableCell>
-                                <TableCell>
-                                    <div className="flex items-center gap-2">
-                                        <Avatar className="border w-6 h-6">
-                                            <AvatarImage alt="Creator's Avatar" src="/placeholder-user.jpg" />
-                                            <AvatarFallback>CR</AvatarFallback>
-                                        </Avatar>
-                                        <span>Creator Name</span>
-                                    </div>
-                                </TableCell>
-                                <TableCell>
-                                    <Badge className="bg-green-500 text-white">Active</Badge>
-                                </TableCell>
-                            </TableRow>
+                            {rooms.map((room, index) => (
+                                <TableRow key={room.id}>
+                                    <TableCell className="font-medium"><Link href={`/room/${room.id}`}>{room.name} </Link></TableCell>
+                                    <TableCell>{room.description}</TableCell>
+                                    <TableCell>
+                                        <div className="flex items-center gap-2">
+                                            <Avatar className="border w-6 h-6">
+                                                <AvatarImage alt="Creator's Avatar" src="/placeholder-user.jpg" />
+                                                <AvatarFallback>CR</AvatarFallback>
+                                            </Avatar>
+                                            <span>{room.roomOwner}</span>
+                                        </div>
+                                    </TableCell>
+                                    <TableCell>
+                                        {
+                                            room.isActive
+                                                ? <Badge className="bg-green-500 text-white"> OnGoing </Badge>
+                                                : <Badge className="bg-red-500 text-white"> Ended </Badge>
+                                        }
+                                    </TableCell>
+                                    <TableCell>{room.targetTime.toLocaleString()}</TableCell>
+                                </TableRow>
+                            ))
+                            }
                         </TableBody>
                     </Table>
                     <div className="flex items-center justify-center">
-                        <Button className="mx-1" size="sm" variant="outline">
+                        <Button className="mx-1" size="sm" variant="outline" onClick={() => turnPageHandler("prev")}
+                            disabled={hasPrevPage}>
                             Prev
                         </Button>
-                        <span>Page 1/10</span>
-                        <Button className="mx-1" size="sm" variant="outline">
+                        <span>Page {currentPage + 1}/{totalPage}</span>
+                        {hasPrevPage}{hasNextPage}
+                        <Button className="mx-1" size="sm" variant="outline" onClick={() => turnPageHandler("next")}
+                            disabled={hasNextPage}>
                             Next
                         </Button>
                     </div>
--- lib/roomService.ts
+++ lib/roomService.ts
@@ -6,6 +6,14 @@ export async function getRoomDetail(data: { roomId: string }) {
     return await response.json();
 }
 
+export async function getRooms({ page, size }: { page: number, size: number }) {
+    const response = await fetch(`/api/rooms`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    })
+    return await response.json();
+}
+
 export async function createRoom(data: { name: string, roomOwner: string, targetTime: string, description: string }) {
     const response = await fetch(`/api/room`, {
         method: 'POST',
@@ -13,4 +21,41 @@ export async function createRoom(data: { name: string, roomOwner: string, target
         body: JSON.stringify(data)
     })
     return await response.json();
+}
+
+
+
+export interface PagingRoom {
+    content:          Room[];
+    pageable:         Pageable;
+    last:             boolean;
+    totalPages:       number;
+    totalElements:    number;
+    size:             number;
+    number:           number;
+    sort:             any;
+    first:            boolean;
+    numberOfElements: number;
+    empty:            boolean;
+}
+
+export interface Room {
+    createdAt:           Date;
+    updatedAt:           Date;
+    id:                  number;
+    name:                string;
+    description:         string;
+    targetTime:          Date;
+    locationSubmissions: any[];
+    isActive:            boolean;
+    roomOwner:           null;
+}
+
+export interface Pageable {
+    pageNumber: number;
+    pageSize:   number;
+    sort:       any;
+    offset:     number;
+    paged:      boolean;
+    unpaged:    boolean;
 }
